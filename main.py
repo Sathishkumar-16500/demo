d06781c0fe299535cd18265a5c6c7f4ea20913d2
Sathishkumar-16500 <sathishr16500@gmail.com>
2023-03-07 10:44:07 +0530

    demo

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def demo():
+    print('hello')
+
+demo()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
